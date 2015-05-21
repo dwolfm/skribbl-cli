0f7ac0a869ec0755dc2fdd16f85f572197a417ed
dwolfm <dwolfm@gmail.com>
2015-05-20 22:57:06 -0700

    added data/**/* to .gitignore

--- skribbl-cli.js
+++ skribbl-cli.js
@@ -38,6 +38,7 @@ function loopback(input, validateArray, back){
 	}
 	return;
 }
+
 function runLoginvsCreate(){
 	console.log('yo dawg, you haven\'t loged in b4!'.rainbow);
 	console.log('---> l for login'.red);
@@ -100,47 +101,3 @@ function runLogin(){
 }
 
 checkForToken();
-// check user token 
-// 		on fail prompt login or create user
-//		on success login
-
-// create user
-// - propt unique username
-// - prompt unique email
-// - prompt password
-// 		on fail retry
-//		on success try login
-
-// login 
-// - if comeing from create user
-// - send username password
-// - if login in with pre existing acount
-// - prompt for username 
-// - prompt for password
-// 		on fail tell user 
-// 		on success store token
-
-// menu
-// - prompt user if the want to fetch story, browse, or profile
-// 		on fetch story go to requset /api/skribbl
-// 		on brows go to /api/story
-// 		on profile go to /api/timeline
-
-// request skribble start
-// - ask for skribbl start
-// 		on fail tell user && re ask
-// 		on succes print first level && options
-// - ask user to keep reading or fork
-// 		on fork ask user for some writing
-// 				on fail make user retry
-// 				on success request return to menu
-// 		on read further print next and repete prompt
-
-
-
-
-//createUser(skribblURL, 'garsdlfkbol', 'lullskdjfwat@wart.glob', 'hahah', function(err, data){
-	//if (err) console.log(data);
-	//console.log('donezies');
-//});
-//
